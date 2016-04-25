cbe1d6a66935a6bc740b7ed61335c5466f182726
mihail.prozorov@progforce.com <mihail.prozorov@progforce.com>
2016-04-25 12:33:39 +0300

    Init/Load to git

---
+++ RandomArrayGenerate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomArrayGenerate
+{
+    class Program
+    {
+        private const int defaultSize = 10 * 1000;
+        private const string defaultFileNamePattern = "RandomArray-{0}.txt";
+
+        static void Main(string[] args)
+        {
+            var arrayGenerator = new RandomArrayGenerator(defaultSize);
+            var randomArray = arrayGenerator.Generate();
+
+            var resultString = string.Join(",", randomArray);
+            var dateTimeToPath = DateTime.Now.ToString("hh-mm-ss dd-MM-yyyy");
+            var fileName = string.Format(defaultFileNamePattern, dateTimeToPath);
+
+            File.WriteAllText(fileName, resultString);
+        }
+    }
+}
---
+++ RandomArrayGenerate/RandomArrayGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomArrayGenerate
+{
+    public class RandomArrayGenerator
+    {
+        private const int valueFromIndexIncrement = 1;
+        private int _size;
+
+        public RandomArrayGenerator(int size)
+        {
+            _size = size;
+        }
+
+        public int[] Generate()
+        {
+            Random _random = new Random();
+            return Enumerable.Range(0, _size)
+                        .Select(sortValue => _random.Next(_size))
+                        .Select((sortValue, index) => new { randomValue = index, sortValue = sortValue })
+                        .OrderBy(item => item.sortValue)
+                        .Select(item => item.randomValue + valueFromIndexIncrement)
+                        .ToArray();
+        }
+
+
+    }
+}
